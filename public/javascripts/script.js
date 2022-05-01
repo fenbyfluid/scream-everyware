0262f7a4308d35702e958957a553d75de55834ff
Erin <fenbyfluid@gmail.com>
2022-05-01 22:35:20 +0100

    Finish reverse engineering the API

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,47 @@
 /** @var {?SerialPort} */
 let port = null;
 
+const modes = {
+    0x00: 'Torment',
+    0x01: 'Smooth Suffering',
+    0x02: 'Bitch Training',
+    0x03: 'Turbo Thruster',
+    0x04: 'Random',
+    0x05: 'Random Bitch',
+    0x06: 'Purgatory',
+    0x07: 'Purgatory Chaos',
+    0x08: 'Persistent Pain',
+    0x09: 'Pulse',
+    0x0A: 'Ramp Pulse',
+    0x0B: 'Ramp Repeat',
+    0x0C: 'Ramp Intensity',
+    0x0D: 'Audio Attack',
+    0x0E: 'Torment (LV)',
+    0x0F: 'Power Waves (LV)',
+    0x10: 'Speed Waves',
+    0x11: 'Demon Play',
+    0x80: 'Extreme Torment',
+    0x81: 'Extreme Bitch Training',
+};
+
 const knownVariables = {
-    '63': 'Trigger Button ???', // 0x0F if trigger mode != manual, or trigger button pushed in manual mode
-    '66': 'Pulse Rate', // 0x00 - 0xFF
-    '6C': 'Input Voltage', // 0x7B = 123 = 12.3V
-    '6D': 'Mode ???', // 0x0E Torment (LV), 0x00 Torment
-    '70': 'Pulse Width', // 0x01 Short, 0x00 Normal, 0x03 Medium, 0x02 Long
-    '72': 'Trigger Rate', // 0x00 - 0xFF
-    '74': 'Trigger Mode', // 0x01 Cont, 0x00 Pulse, 0x03 Manual, 0x02 Mic
-    '7A': 'Buzzer Mode', // 0x00 Always, 0x01 When Output
+    '1': 'Switch Mode Short',
+    '2': 'Switch Mode Normal',
+    '3': 'Switch Mode Medium',
+    '4': 'Switch Mode Long',
+    'c': 'Enabled Channels', // 0x01 Channel 1, 0x02 Channel 2, 0x04 Channel 3, 0x08 Channel 4
+    'd': 'Count Down Timer', // Seconds Remaining
+    'f': 'Pulse Rate', // 0x00 - 0xFF
+    'i': 'Mode Info', // Purgatory Level
+    'l': 'Input Voltage', // 0x7B = 123 = 12.3V
+    'm': 'Mode',
+    'p': 'Pulse Width', // 0x01 Short, 0x00 Normal, 0x03 Medium, 0x02 Long
+    'r': 'Trigger Rate', // 0x00 - 0xFF
+    's': 'Firmware Version', // Queried at connect, 0x14 = 20 = 2.0
+    't': 'Trigger Mode', // 0x01 Cont, 0x00 Pulse, 0x03 Manual, 0x02 Mic
+    'u': 'Unit Mode', // 0x00 Normal, 0x01 Extreme (Double Voltage)
+    'v': 'Output Level', // Not sent automatically, can be queried
+    'z': 'Buzzer Mode', // 0x00 Always, 0x01 When Output
 };
 
 /**
@@ -62,28 +94,32 @@ document.getElementById('start').addEventListener('click', async () => {
         responseBuffer.push(...buffer.value);
 
         for (;;) {
-            let responseEnd = responseBuffer.indexOf(0x0A);
+            if (responseBuffer.length < 3) {
+                break;
+            }
+
+            // Messages are always >= 3 bytes, 0x0A can appear as the 2nd byte so skip it.
+            let responseEnd = responseBuffer.indexOf(0x0A, 2);
             if (responseEnd === -1) {
                 break;
             }
 
-            // 0x0A can appear in the response, if it's a 3-byte
-            // (standard) response override the end position.
-            if (responseBuffer[2] === 0x0A) {
-                responseEnd = 2;
-            }
-
             const response = responseBuffer.splice(0, responseEnd + 1);
             console.log('read ', formatBytes(response));
-            // console.log('     ', response.map(b => (b >= 32 && b < 127) ? String.fromCharCode(b) : '.').join(''));
 
-            const key = response[0].toString(16).padStart(2, '0').toUpperCase();
+            // Responses > 3 bytes are string messages.
+            if (response.length > 3) {
+                console.log('     ', response.map(b => (b >= 32 && b < 127) ? String.fromCharCode(b) : '.').join(''));
+                break;
+            }
+
+            const key = String.fromCharCode(response[0]);
             let variable = document.querySelector(`#variables > dd[data-key="${key}"]`)
 
             if (!variable) {
                 const variables = document.getElementById('variables');
                 const variableKey = document.createElement('dt');
-                variableKey.textContent = `0x${key} - ${knownVariables[key] || 'Unknown'}`;
+                variableKey.textContent = `${key} - ${knownVariables[key] || 'Unknown'}`;
                 variables.appendChild(variableKey);
                 variable = document.createElement('dd');
                 variable.setAttribute('data-key', key);
