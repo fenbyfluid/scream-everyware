75b14403149d2a622205a3e1cfe5c80030dbd77f
Erin <fenbyfluid@gmail.com>
2022-05-04 23:23:30 +0100

    Update branding

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
-import { Code } from "@blueprintjs/core";
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <Code>src/App.tsx</Code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      Hello, World.
     </div>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { App } from './App';
+import { Classes, FocusStyleManager } from '@blueprintjs/core';
+
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+if (prefersDarkScheme.matches) {
+  document.body.classList.add(Classes.DARK);
+}
+
+prefersDarkScheme.addEventListener('change', ev => {
+  document.body.classList.toggle(Classes.DARK, ev.matches);
+});
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
